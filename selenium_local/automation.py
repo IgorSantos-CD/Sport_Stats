50f1f13c34c275c6f9a38049189e8c7f89bb65f8
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-12 10:55:26 -0300

    feat: driver inicia minimizado e sem logs

--- selenium_local/automation.py
+++ selenium_local/automation.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
 
-def iniciar_driver():
-    driver = webdriver.Edge()
+def iniciar_driver(log_level = 3):
+    options = Options()
+    options.add_argument(f"--log-level={log_level}")
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("--headless")
+
+    service = Service(executable_path="./webdrivers/msedgedriver.exe")
+    driver = webdriver.Edge(options=options, service=service)
     return driver
