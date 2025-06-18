7f5a1bc5376331d6f8da26f18b49539ad50d8921
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-18 15:03:05 -0300

    feat: coletando estatisticas de partidas unicas

--- collector/main.py
+++ collector/main.py
@@ -1,4 +1,9 @@
-from scrapers.scraper_matches import coletar_partidas_temporada
+from scrapers.scraper_matches import coletar_partidas_temporada_lote, coletar_partidas_temporada
+from scrapers.scraper_stats import coletar_match_stats_selenium
+from utils.date_utils import format_stat_name, map_period_to_half
+from selenium_local.automation import iniciar_driver
+from database.db_connection import conectar_banco
+from database.db_actions import inserir_dados, select_dados
 import pandas as pd
 import time
 
@@ -6,12 +11,31 @@ import time
 if __name__ == "__main__":
 
     start = time.time()
-    #Campeonato Brasileiro 2024
-    base = pd.read_csv(r"C:\Users\VIPEXGRUNT182\Desktop\Igor\Vs Code\Pessoal\Sport_Stats\output\Seasons.csv")
+    
+    # COLETAR PARTIDAS DAS TEMPORADAS
+    #base = pd.read_csv(r"C:\Users\VIPEXGRUNT182\Desktop\Igor\Vs Code\Pessoal\Sport_Stats\output\Seasons.csv")
 
-    df = coletar_partidas_temporada(base)
+    #df = coletar_partidas_temporada_lote(base)
 
-    df.to_csv('./output/partidas_temporada.csv', index=False)
+    #df.to_csv('./output/partidas_temporada.csv', index=False)
+
+    conn = conectar_banco()
+
+    data = select_dados(conn, 'matches', ['id','home_team_id','away_team_id'], fetch='one')
+    
+    match_id = data[0]
+    home_team_id = data[1]
+    away_team_id = data[2]
+
+    df_stats = coletar_match_stats_selenium(match_id, home_team_id, away_team_id)
+
+    print(df_stats.head())
+
+    '''if df_stats is not None:
+        df_stats.to_csv('./output/stats_game.csv')
+    else:
+        print("Nenhuma estatística coletada.")'''
+
     
     end = time.time()
 
