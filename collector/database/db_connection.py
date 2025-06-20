1d4311f5031a231a51c22554e558bc0df5f78f9d
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-20 18:03:04 -0300

    fix: ajustando funções de ações e conexão do banco

--- collector/database/db_connection.py
+++ collector/database/db_connection.py
@@ -12,27 +12,3 @@ def conectar_banco():
         port='5432'
     )
     return conn
-
-def inserir_dados(tabela, dataframe, conn):
-    cursor = conn.cursor()
-
-    colunas = list(dataframe.columns)
-    valores = [tuple(x) for x in dataframe.to_numpy()]
-
-    insert_query = sql.SQL(
-        "INSERT INTO {tabela} ({campos}) VALUES ({placeholders})"
-    ).format(
-        tabela = sql.Identifier(tabela),
-        campos = sql.SQL(', ').join(map(sql.Identifier, colunas)),
-        placeholders = sql.SQL(', ').join(sql.Placeholder()*len(colunas))
-    )
-
-    try:
-        cursor.executemany(insert_query.as_string(conn), valores)
-        conn.commit()
-        print(f"Dados inseridos na tabela {tabela}")
-    except Exception as e:
-        conn.rollback()
-        print(f"Erro ao inserir na tabela {tabela}: {e}")
-    finally:
-        cursor.close()
