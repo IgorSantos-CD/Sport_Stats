a8ee3b4329ad00bae2f795390c5c132ab3ec8915
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-20 18:04:17 -0300

    fix: ajustando função de inicialização de driver

--- collector/selenium_local/automation.py
+++ collector/selenium_local/automation.py
@@ -4,9 +4,12 @@ from selenium.webdriver.edge.service import Service
 
 def iniciar_driver(log_level = 3):
     options = Options()
-    options.add_argument(f"--log-level={log_level}")
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    options.add_argument("--headless")
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--log-level=3")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-blink-features=AutomationControlled")
 
     service = Service(executable_path="./webdrivers/msedgedriver.exe")
     driver = webdriver.Edge(options=options, service=service)
