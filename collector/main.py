9450b893a76d5247c60f208823a50ace40a4d6f0
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-21 08:48:50 -0300

    fix: ajustando a main para coletar jogos

--- collector/main.py
+++ collector/main.py
@@ -1,6 +1,6 @@
 from scrapers.scraper_matches import coletar_partidas_temporada_lote, coletar_partidas_temporada
-from scrapers.scraper_stats import coletar_match_stats_selenium
-from utils.date_utils import format_stat_name, map_period_to_half
+from scrapers.scraper_stats import coletar_match_stats_selenium, coletar_stats_em_lote
+from utils.date_utils import format_stat_name, map_period_to_half, trata_stats
 from selenium_local.automation import iniciar_driver
 from database.db_connection import conectar_banco
 from database.db_actions import inserir_dados, select_dados
@@ -11,32 +11,22 @@ import time
 if __name__ == "__main__":
 
     start = time.time()
-    
-    # COLETAR PARTIDAS DAS TEMPORADAS
-    #base = pd.read_csv(r"C:\Users\VIPEXGRUNT182\Desktop\Igor\Vs Code\Pessoal\Sport_Stats\output\Seasons.csv")
+    conn = conectar_banco()
 
-    #df = coletar_partidas_temporada_lote(base)
+    data = select_dados(conn, 'matches', ['id', 'home_team_id', 'away_team_id'])
 
-    #df.to_csv('./output/partidas_temporada.csv', index=False)
+    conn.close()
+
+    df = coletar_stats_em_lote(data)
+
+    df_tratado = trata_stats(df)
 
     conn = conectar_banco()
 
-    data = select_dados(conn, 'matches', ['id','home_team_id','away_team_id'], fetch='one')
-    
-    match_id = data[0]
-    home_team_id = data[1]
-    away_team_id = data[2]
+    inserir_dados("match_stats",df_tratado, conn)
 
-    df_stats = coletar_match_stats_selenium(match_id, home_team_id, away_team_id)
+    conn.close()
 
-    print(df_stats.head())
-
-    '''if df_stats is not None:
-        df_stats.to_csv('./output/stats_game.csv')
-    else:
-        print("Nenhuma estatística coletada.")'''
-
-    
     end = time.time()
 
     print(f"Tempo de execução: {end-start:.2f} segundos")
