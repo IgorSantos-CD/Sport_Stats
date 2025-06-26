9308ff45e0ba2eae0d645556f35a80b229aaf2d5
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-26 17:08:40 -0300

    feat: melhorando funções de inserção no banco

--- collector/database/db_actions.py
+++ collector/database/db_actions.py
@@ -6,26 +6,33 @@ import psycopg2
 from psycopg2 import sql
 
 
-def inserir_dados(tabela, dados):
+def inserir_dados(tabela, dados, chave_conflito='id'):
     conn = conectar_banco()
     cursor = conn.cursor()
 
     #GERA STRING COM OS NOMES DAS COLUNAS
-    colunas = ",".join(dados.columns)
+    colunas = list(dados.columns)
+    colunas_str = ",".join(colunas)
 
     #CRIA UM TUPLO PARA CADA LINHA DO DATAFRAME
     valores = [tuple(x) for x in dados.to_numpy()]
 
+    #GERA SET PARA COLUNAS QUE SERÃO ATUALIZADAS (EXCLUINDO A CHAVE DE CONFLITO)
+    colunas_para_update = [col for col in colunas if col != chave_conflito]
+    set_update = ", ".join([f"{col} = EXCLUDED.{col}" for col in colunas_para_update])
+
 
     query = f"""
-    INSERT INTO {tabela} ({colunas})
+    INSERT INTO {tabela} ({colunas_str})
     VALUES %s
-    ON CONFLICT (id) DO NOTHING
+    ON CONFLICT ({chave_conflito}) DO UPDATE SET
+    {set_update}
     """
 
     try:
         execute_values(cursor,query,valores)
         conn.commit()
+        print("UPSERT concluído com sucesso.")
     except Exception as e:
         conn.rollback()
         print("Erro ao inserir os dados:", e)
@@ -56,4 +63,5 @@ def executar_query(query, fetch='all'):
         print(f"Erro ao executar a query: {e}")
         return None
     finally:
+        cursor.close()
         conn.close()
