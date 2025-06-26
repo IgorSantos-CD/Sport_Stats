18b8ab9947b714bdd892090efe43ea04dae9b1e2
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-26 17:06:32 -0300

    feat: melhorando organização do código

--- collector/main.py
+++ collector/main.py
@@ -6,34 +6,28 @@ from database import executar_query, inserir_dados
 if __name__ == "__main__":
     start = time.time()
 
-    '''#COLETANDO PAISES DISPONIVEIS PARA CONSULTA
-    df_paises = coletar_paises()
+    #BUSCANDO INFORMAÇÕES NO BANCO DE DADOS
     bd_paises = executar_query('SELECT * FROM countries')
-    if len(df_paises) == len(bd_paises):
-        print("Registros Atualizados!")
-    else:
-        df_dif = df_paises.loc[~df_paises['id'].isin(bd_paises['id'])].copy()
-        inserir_dados('countries', df_dif)
+    bd_competitions = executar_query('SELECT * FROM competitions')
+    bd_seasons = executar_query('SELECT * FROM seasons')
+    bd_rounds = executar_query('SELECT * FROM rounds')
+    bd_teams = executar_query('SELECT * FROM teams')
+    bd_matches = executar_query('SELECT * FROM matches')
+    bd_rounds = executar_query('SELECT * FROM rounds')
 
-    #COLETANDO COMPETIÇÕES DISPONÍVEIS PARA CONSULTA
+
+    #COLETANDO INFORMAÇÕES VIA API
+    df_paises = coletar_paises()
     df_competitions = coletar_competicoes()
-    bd_competitions = executar_query('SELECT * FROM competitions', fetch='all')
-    if len(df_competitions) == len(bd_competitions):
-        print("Registros Atualizados!")
-    else:
-        df_dif = df_competitions.loc[~df_competitions['id'].isin(bd_competitions['id'])].copy()
-        inserir_dados('competitions', df_dif)
-
     df_seasons = coletar_seasons()
-    bd_seasons = executar_query('SELECT * FROM seasons', fetch='all')
-    if len(df_seasons) == len(bd_seasons):
-        print("Registros Atualizados!")
-    else: 
-        df_dif = df_seasons.loc[~df_seasons['id'].isin(bd_seasons['id'])].copy()
-        inserir_dados('seasons', df_dif)'''
-    
-    coletar_rodadas()
+    df_rounds = coletar_rodadas()
 
+
+    atualizar_db(df_paises, bd_paises, 'countries')
+    atualizar_db(df_competitions, bd_competitions,'competitions')
+    atualizar_db(df_seasons, bd_seasons, 'seasons')
+
+    atualizar_registros(df_rounds, bd_rounds, 'rounds', ['id_season', 'id_competition'])
 
     end = time.time()
 
