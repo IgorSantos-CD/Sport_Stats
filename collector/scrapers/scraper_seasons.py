121e5b77c3013f104f572eda7bca148c2edab5e8
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-26 18:05:11 -0300

    feat: otimizando coleta de seasons

--- collector/scrapers/scraper_seasons.py
+++ collector/scrapers/scraper_seasons.py
@@ -5,14 +5,13 @@ from utils import transformar_json, gerar_dataframe, delay_aleatorio
 from tqdm import tqdm
 
 def coletar_seasons():
+    driver = iniciar_driver()
     competicoes = executar_query('SELECT id FROM competitions', fetch='all')['id']
     seasons = []
     try:
         for comp in tqdm(competicoes):
-            driver = iniciar_driver()
             driver.get(f"https://api.sofascore.com/api/v1/unique-tournament/{comp}/seasons")
-            #delay_aleatorio(1.5,1.8)
-            
+            delay_aleatorio(1.5,2.0)
             pre = driver.find_element(By.TAG_NAME, 'pre').text
             json_seasons = transformar_json(pre)
         
@@ -25,15 +24,13 @@ def coletar_seasons():
                     'editor' : str(season.get('editor', 'false')).lower() == 'true' 
                 }
                 seasons.append(info)
-            driver.quit()
     except Exception as e:
         print(f"Não foi possivel coletar informações da competição: {comp}\n")
         print(pre)
         driver.quit()
 
-    
-    df = gerar_dataframe(seasons, colunas=['id', 'name', 'year', 'editor', 'id_competition'])
     driver.quit()
+    df = gerar_dataframe(seasons, colunas=['id', 'name', 'year', 'editor', 'id_competition'])
 
     return df
     
