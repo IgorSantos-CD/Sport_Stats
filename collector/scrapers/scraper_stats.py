53657ed0d794f76ec379be92623b2c9dcfdd6309
IgorSantos-CD <igor.5antos@outlook.com>
2025-06-27 17:57:17 -0300

    feat: criando função de coletar stats

--- collector/scrapers/scraper_stats.py
+++ collector/scrapers/scraper_stats.py
@@ -1,69 +1,75 @@
-from selenium_local.automation import iniciar_driver
-from database.db_connection import conectar_banco
-from database.db_actions import inserir_dados
-from utils.date_utils import parse_statistics
-from utils.scraping_ultis import delay_aleatorio, retry
+from collector.selenium_local import iniciar_driver
+from database import executar_query
+from collector.utils import parse_stats_partida, delay_aleatorio,transformar_json, atualizar_db
+from selenium.webdriver.common.by import By
 from tqdm import tqdm
 import json
 import pandas as pd
 import time
+    
+def coletar_stats_partida():
+    #BUSCA TODOS OS JOGOS FINALIZADOS COM OS TIMES
+    query = """
+        SELECT id, home_team_id, away_team_id
+        FROM matches
+        WHERE status = 'Ended';
+    """
 
-def coletar_match_stats_selenium(match_id, home_team_id, away_team_id):
+    df_matches = executar_query(query, fetch="all")
+
+    # BUSCA ESTATS JÁ EXISTENTES NO BANCO
+    query_stats_existentes = """
+        SELECT match_id, team_id, stat_name, half
+        FROM match_stats;
+    """
+    df_existentes = executar_query(query_stats_existentes, fetch="all")
+    df_existentes["assinatura"] = (
+        df_existentes["match_id"].astype(str) + "-" +
+        df_existentes["team_id"].astype(str) + "-" +
+        df_existentes["stat_name"] + "-" +
+        df_existentes["half"].astype(str)
+    )
+    assinaturas_existentes = set(df_existentes["assinatura"])
 
     driver = iniciar_driver()
-    url = f"https://www.sofascore.com/api/v1/event/{match_id}/statistics"
+    for row in tqdm(df_matches.head(250).itertuples(index=False), desc="Estatisticas"):
+        match_id = row.id
+        home_team_id = row.home_team_id
+        away_team_id = row.away_team_id
 
-    try:
-        driver.get(url)
-
-        pre = driver.find_element("tag name", "pre").text
-        data = json.loads(pre)
-
-        registros = parse_statistics(data, match_id,home_team_id, away_team_id)
-
-        df = pd.DataFrame(registros)
-        return df
-
-    except Exception as e:
-        print(f"Erro ao coletar stats do match {match_id}: {e}")
-        return None
-
-
-def coletar_stats_em_lote(partidas):
-    driver = iniciar_driver()
-    todos_registros = []
-
-    for partida in tqdm(partidas):
-        match_id = partida[0]
-        home_team_id = partida[1]
-        away_team_id = partida[2]
-        url = f"https://www.sofascore.com/api/v1/event/{match_id}/statistics"
-        driver.get(url)
-        delay_aleatorio(1.5, 3.5)
         try:
-            pre = driver.find_element("tag name", "pre").text
-            data = json.loads(pre)
-            registros =  parse_statistics(data, match_id, home_team_id, away_team_id)
+            url = f"https://www.sofascore.com/api/v1/event/{match_id}/statistics"
+            driver.get(url)
+            delay_aleatorio(1.5,1.9)
 
-            if registros:
-                todos_registros.extend(registros)
-                delay_aleatorio(1.5,3.5)
+            pre = driver.find_element(By.TAG_NAME, 'pre').text
+            json_data = transformar_json(pre)
+
+            stats = parse_stats_partida(json_data, match_id, home_team_id, away_team_id)
+            if stats:
+                df_stats = pd.DataFrame(stats)
+                # GERA ASSINATURA PARA REMOVER DUPLICADOS
+                df_stats["assinatura"] = (
+                    df_stats["match_id"].astype(str) + "-" +
+                    df_stats["team_id"].astype(str) + "-" +
+                    df_stats["stat_name"] + "-" +
+                    df_stats["half"].astype(str)
+                )
+                df_stats = df_stats[~df_stats["assinatura"].isin(assinaturas_existentes)].copy()
+                df_stats.drop(columns=["assinatura"], inplace=True)
+                if not df_stats.empty:
+                    atualizar_db(df_stats, 'match_stats')
+                    print(f"[OK] Match {match_id} - {len(stats)} stats inseridas")
+                else:
+                    print(f"[SKIP] Match {match_id} - Nenhuma stat nova")
             else:
-                print(f"Match {match_id} sem estatisticas disponíveis")
-
+                print(f"[SEM DADOS] Match {match_id}")
+            
+            
         except Exception as e:
-            print(f"Erro no match {match_id}: {e}")
+            print(f"[ERRO] Match {match_id}: {e}")
 
     driver.quit()
-
-    if todos_registros:
-        df = pd.DataFrame(todos_registros)
-        df.to_csv('./output/stats_game.csv', index=False)
-        print("CSV gerado com sucesso.")
-        return df
-    else:
-        print("Nenhum dado de estatística foi coletado.")
-        return pd.DataFrame()  # Retorna DataFrame vazio se nada for coletado
 
 
         
