4f31a2b271dda2158cd6e6501119cd78a5124543
IgorSantos-CD <igor.5antos@outlook.com>
2025-07-04 18:08:27 -0300

    fix: melhorando organização do código

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
-from collector.scrapers import coletar_paises, coletar_competicoes,coletar_seasons,coletar_rodadas, coletar_partidas_por_rodada,coletar_stats_partida
-from collector.utils import buscar_partidas_para_coletar_stats, parse_stats_partida
+from collector.scrapers import coletar_paises, coletar_competicoes,coletar_seasons,coletar_rodadas, coletar_partidas_por_rodada,coletar_stats_partida,coleta_por_data
+from collector.utils import buscar_partidas_para_coletar_stats, atualizar_db
 from database import executar_query, inserir_dados, conectar_banco_nuvem
 #from utils import atualizar_db, atualizar_registros
 
@@ -9,49 +9,24 @@ from database import executar_query, inserir_dados, conectar_banco_nuvem
 if __name__ == "__main__":
     start = time.time()
 
-    '''#BUSCANDO INFORMAÇÕES NO BANCO DE DADOS
-    bd_paises = executar_query('SELECT * FROM countries')
-    bd_competitions = executar_query('SELECT * FROM competitions')
-    bd_seasons = executar_query('SELECT * FROM seasons')
-    bd_rounds = executar_query('SELECT * FROM rounds')
-    bd_teams = executar_query('SELECT * FROM teams')
-    bd_matches = executar_query('SELECT * FROM matches')
-    bd_rounds = executar_query('SELECT * FROM rounds')
-    bd_stats = executar_query('SELECT * FROM match_stats)
 
+    #COLETANDO INFORMAÇÕES VIA API DE PARTIDAS DO DIA
+    dados = coleta_por_data()
 
-    #COLETANDO INFORMAÇÕES VIA API
-    df_paises = coletar_paises()
-    df_competitions = coletar_competicoes()
-    df_seasons = coletar_seasons()
-    df_rounds = coletar_rodadas()
-    df_matches, df_teams = coletar_partidas_por_rodada()
+    dados['countries'].to_csv('./output/countries_2.csv')
+    dados['unique_competitions'].to_csv('./output/unique_competitions_2.csv')
+    dados['competitions'].to_csv('./output/competitions_2.csv')
+    dados['seasons'].to_csv('./output/seasons_2.csv')
+    dados['teams'].to_csv('./output/teams_2.csv')
+    dados['matches'].to_csv('./output/matches_2.csv')
 
-    atualizar_db(df_paises, bd_paises, 'countries')
-    atualizar_db(df_competitions, bd_competitions,'competitions')
-    atualizar_db(df_seasons, bd_seasons, 'seasons')
-
-    atualizar_registros(df_rounds, bd_rounds, 'rounds', ['id_season', 'id_competition'])
-    
-    
-    atualizar_registros(df_teams,bd_teams,'teams',chave_conflito='id')
-    atualizar_registros(df_matches, bd_matches, 'matches', chave_conflito='id')'''
-
-    #coletar_stats_partida()
-
-    bd_matches = executar_query('SELECT id FROM matches')
-    bd_stats = executar_query('SELECT match_id FROM match_stats')
-
-    bd_stats = bd_stats.drop_duplicates(subset='match_id')
-    n_coletados = []
-    for i in bd_matches['id']:
-        if i not in  list(bd_stats['match_id']):
-            n_coletados.append(i)
-            print("Verificação Concluída")
-    
-    print(len(n_coletados))
-
-    
+    atualizar_db(dados['countries'], "countries",'id')
+    atualizar_db(dados['unique_competitions'], "unique_competitions",'id')
+    atualizar_db(dados['competitions'], "competitions",'slug_name')
+    atualizar_db(dados['seasons'], "seasons",'id')
+    atualizar_db(dados['teams'], "teams",'id')
+    atualizar_db(dados['matches'], "matches",'id')
+ 
 
     end = time.time()
 
